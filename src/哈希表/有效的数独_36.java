5da88c3df2e73ee45fcb93f776dda552da975185
xiao <m18836183092@163.com>
2019-01-24 18:51:29 +0800

    第一次提交

---
+++ src/哈希表/有效的数独_36.java
@@ -0,0 +1,16 @@
+package 哈希表;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings("ALL")
+public class 有效的数独_36 {
+    /**
+     * 判断一个数独是否有效
+     * @param board
+     * @return
+     */
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+}
