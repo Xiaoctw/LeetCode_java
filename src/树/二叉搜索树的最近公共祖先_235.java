b1f4ae4d567d88368c6aeeebbf0d9ed05bd90804
xiao <m18836183092@163.com>
2019-01-25 08:35:16 +0800

    第五次提交

--- src/树/二叉搜索树的最近公共祖先_235.java
+++ src/树/二叉搜索树的最近公共祖先_235.java
@@ -11,56 +11,25 @@ public class 二叉搜索树的最近公共祖先_235 {
     /**
      * 寻找到两个节点的最近公共祖先
      * 一个节点可以是它自己的祖先
-     *
+     *这道题应该使用二叉搜索树的特性解决，不是遍历
      * @param root
      * @param p
      * @param q
      * @return
      */
+    TreeNode res = null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Queue<TreeNode> nodes = new LinkedList<>();
-        TreeNode res = null;
-        nodes.offer(root);
-        while (!nodes.isEmpty()) {
-            TreeNode node = nodes.poll();
-            if (judge(node, p, q)) {
-                res = node;
-            }
-            if (res == p || res == q) {
-                return res;
-            }
-            if (node.left != null) {
-                nodes.offer(node.left);
-            }
-            if (node.right != null) {
-                nodes.offer(node.right);
-            }
-        }
-        return null;
+        find(root, p, q);
+        return res;
     }
 
-    private boolean judge(TreeNode node, TreeNode p, TreeNode q) {
-        boolean hasP = false;
-        boolean hasQ = false;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(node);
-        while (!stack.isEmpty()) {
-            TreeNode node1 = stack.pop();
-            if (node1 == p) {
-                hasP = true;
-            } else if (node1 == q) {
-                hasQ = true;
-            }
-            if (hasP && hasQ) {
-                return true;
-            }
-            if (node1.left != null) {
-                stack.push(node1.left);
-            }
-            if (node1.right != null) {
-                stack.push(node1.right);
-            }
+    private void find(TreeNode node, TreeNode p, TreeNode q) {
+        if ((node.val - p.val) * (node.val - q.val) <= 0) {
+            res = node;
+        } else if (node.val < p.val && node.val < q.val) {
+            find(node.right, p, q);
+        } else {
+            find(node.left, p, q);
         }
-        return false;
     }
 }
