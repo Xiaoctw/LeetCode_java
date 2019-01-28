f9d6797d09e8c6a5771c78a7c738390ac9bc5780
Xiaoctw <m18846183092@163.com>
2019-01-28 08:52:45 +0800

    第十次提交

--- src/树/根据二叉树创建字符串_606.java
+++ src/树/根据二叉树创建字符串_606.java
@@ -1,27 +1,42 @@
 package 树;
 
 
+import org.junit.Test;
+
 @SuppressWarnings("ALL")
 public class 根据二叉树创建字符串_606 {
 
     public String tree2str(TreeNode t) {
         if (t == null) {
-            return null;
+            return "";
         }
         StringBuilder builder = new StringBuilder();
         builder.append(t.val);
         String left = tree2str(t.left);
-        if (left != null) {
+        String right = tree2str(t.right);
+        if (!left.equals("") && !right.equals("")) {
             builder.append("(");
             builder.append(left);
             builder.append(")");
-        }
-        String right = tree2str(t.right);
-        if (right != null) {
+            builder.append("(");
+            builder.append(right);
+            builder.append(")");
+        } else if (left.equals("") && right.equals("")) {
+        } else if (right.equals("")) {
+            builder.append("(");
+            builder.append(left);
+            builder.append(")");
+        } else {
+            builder.append("()");
             builder.append("(");
             builder.append(right);
             builder.append(")");
         }
         return builder.toString();
+    }
+
+    @Test
+    public void test() {
+
     }
 }
