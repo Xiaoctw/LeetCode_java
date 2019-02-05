8214b8e396919623334a8386e9e9ce528e5b7d52
Xiaoctw <m18846183092@163.com>
2019-02-05 18:29:14 +0800

    第18次提交

--- src/数组/组合总和_39.java
+++ src/数组/组合总和_39.java
@@ -2,9 +2,7 @@ package 数组;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @SuppressWarnings("ALL")
 public class 组合总和_39 {
@@ -15,6 +13,9 @@ public class 组合总和_39 {
      * @param target
      * @return
      */
+    int pre = -1;
+    long time1 = System.currentTimeMillis();
+    long time2 = 0;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> lists = new ArrayList<>();
@@ -27,7 +28,18 @@ public class 组合总和_39 {
             return;
         }
         if (target == 0) {
+            time2 = System.currentTimeMillis();
             List<Integer> temp = new ArrayList<>(list);
+            if (temp.get(0) != pre) {
+                for (int i : temp
+                ) {
+                    System.out.printf("%d ", i);
+                }
+                System.out.println();
+                System.out.println("当前数字执行时间" + (double) (time2 - time1));
+                pre = temp.get(0);
+                time1 = System.currentTimeMillis();
+            }
             lists.add(temp);
         }
         for (int i = index; i < nums.length && nums[i] <= target; i++) {
@@ -40,8 +52,23 @@ public class 组合总和_39 {
     @Test
     public void test() {
         int[] candidates = {2, 3, 6, 7};
-        var res = combinationSum(candidates, 7);
-        System.out.println();
+        int[] candidates1 = {4, 6, 33, 4, 32, 13, 9, 16, 65, 32, 11, 4, 3, 12, 43, 42, 42, 4, 32, 25, 32, 65, 32};
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < candidates1.length; i++) {
+            set.add(candidates1[i]);
+        }
+        int len = set.size();
+        int[] A = new int[len];
+        int i = 0;
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            A[i] = iterator.next();
+            i++;
+        }
+        long time = System.currentTimeMillis();
+        var res = combinationSum(A, 300);
+        long time3 = System.currentTimeMillis();
+        System.out.println("总时间" + (double) (time3 - time));
         return;
     }
 }
